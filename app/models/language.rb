7ab8fda0b3d37254e39aa8ecb46d405a53c30768
tmtocb <kittz.esport@gmail.com>
2021-01-24 13:48:32 +0100

    add Language inclusion validations of LanguageList with TDD

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,3 @@
 class Language < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, inclusion: { in: LanguageList::COMMON_LANGUAGES.map(&:name) }
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe Language, type: :model do
 
   describe '#validations' do
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_inclusion_of(:name).in_array(LanguageList::COMMON_LANGUAGES.map(&:name)) }
   end
 end
