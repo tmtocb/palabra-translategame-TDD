c0fb2e7ce5047773ebe4d1f3120d3666fa4f6045
tmtocb <kittz.esport@gmail.com>
2021-01-25 16:20:36 +0100

    add authorization to Palabra author for edit, update, destroy, refactor tests

---
+++ app/policies/palabra_policy.rb
@@ -0,0 +1,27 @@
+class PalabraPolicy
+  attr_reader :user, :palabra
+
+  def initialize(user, palabra)
+    @user = user
+    @palabra = palabra
+  end
+
+  def update?
+    palabra_belongs_to_user?
+  end
+        
+  def edit?
+    palabra_belongs_to_user?
+  end
+        
+  def destroy?
+    palabra_belongs_to_user?
+  end
+        
+  private
+        
+  def palabra_belongs_to_user?
+    user == palabra.user
+  end
+        
+end
