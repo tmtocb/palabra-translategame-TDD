1b5706f8e2c44b119a89edd8aecb298c14547a86
tmtocb <kittz.esport@gmail.com>
2021-01-26 14:10:48 +0100

    add font-awesome via yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import 'bootstrap/dist/css/bootstrap'
 require("stylesheets/application.scss") 
 require('./nested-forms/addFields')
 require('./nested-forms/removeFields')
+import "@fortawesome/fontawesome-free/css/all"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
