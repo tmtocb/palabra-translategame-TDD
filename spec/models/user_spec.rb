0f81121a01cd5ecd0ff817ca76f707f05ea28985
tmtocb <kittz.esport@gmail.com>
2021-01-27 00:43:20 +0100

    hide part of User email with TDD in index and show Palabra

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'columns' do
+  describe '#columns' do
     it { is_expected.to have_db_column(:email) }
     it { is_expected.to have_db_column(:encrypted_password) }
   end
 
-  describe 'associations' do
+  describe '#associations' do
     it { is_expected.to have_many(:palabras) }
+  end
+
+  describe '.secret_email' do
+    subject { user.secret_email }
+    let(:user) { build_stubbed(:user, email: 'john.test@example.com') }
+    it { is_expected.to eq('john.t###@example.com') }
   end
 end
